51e1e94eb22df296c1c782cef2ef9e3cf9031f66
A_one1111 <541386987@qq.com>
2017-06-17 11:17:23 +0800

    vue-cli脚手架

---
+++ code/04/vue-webpack/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h3>welcome to vue-webpack</h3>
+    <p>会使用（大型公司）EsLink检查书写规范，单元测试等</p>
+    <p>data数据————{{name}}</p>
+    <p>书写规范————{{msg}}</p>
+    <p>引入新的组件，不加;号，key-value冒号后空格</p>
+    <News>1233</News>
+  </div>
+</template>
+
+<script>
+import News from './components/News.vue'
+export default {
+  name: 'app',
+  data () {
+    return {
+      name: 'data()返回的数据对象',
+      msg: '函数名称后空格，小括号大括号间空格，对象冒号间空格'
+    }
+  },
+  components: {
+    News: News
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ code/04/vue-webpack/src/components/News.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+	<h3>这是新闻页面</h3>
+	<p>没有显示是什么原因，还有最后一行要有空行，组件要有<b>根标签</b></p>
+	
+</div>
+</template>
+<script>
+	
+</script>
+<style>
+	
+</style>
