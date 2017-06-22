f5444ee041bfd152cb8726f6c51b8043ef17ffc4
A_one1111 <541386987@qq.com>
2017-06-22 17:29:30 +0800

    添加VUEX与新闻案例

---
+++ code/06-transtion-router/vue-cli/src/components/News.vue
@@ -0,0 +1,28 @@
+<template>
+<section  id='news'>
+	<h2>新闻页</h2>
+	<ul>
+		<li v-for='(val,index) in news'>
+			{{index}}----{{val}}
+		</li>
+	</ul>
+	<div>
+		<router-view>数据</router-view>
+	</div>
+</section>
+</template>
+<script>
+	export default {
+		name:'news',
+		data(){
+			return {
+				news:['ajax','nodejs','angularjs','vuejs','reactjs']
+			}
+		}
+	}
+</script>
+<style scope>
+	li{
+		border: 1px dashed #ccc;
+	}
+</style>
---
+++ code/07-插件/element-demo2/src/element-ui.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+
+// Vue.component(Button.name,Button);
+
+// 按钮和单选....
+import {Button,Radio,DatePicker,Rate,Pagination} from 'element-ui'
+
+Vue.use(Button);
+Vue.use(Radio);
+Vue.use(DatePicker);
+Vue.use(Rate);
+Vue.use(Pagination);
+
+// tabs
+import {TableColumn,Table,Switch,Badge,TabPane,Tabs} from 'element-ui'
+Vue.use(Badge);
+Vue.use(Table);
+Vue.use(TabPane);
+Vue.use(Tabs);
+Vue.use(Switch);
+Vue.use(TableColumn);
